c3f71a8e26c822612174765898fb9f324ef5414f
Jiwon Hae <hjiwon1994@gmail.com>
2025-07-19 02:07:45 -0400

    chore: Update import pathing

--- data/collectors/wikipedia.py
+++ data/collectors/wikipedia.py
@@ -9,15 +9,15 @@ from typing import Optional
 from dateutil import parser
 
 try:
-    from ..models.info import *
+    from ...ingestion.models import *
 except ImportError:
-    import os
     import sys
+    import os
+    sys.path.insert(0, os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", "..")
+    ))
     
-    sys.path.append(os.path.dirname(
-        os.path.dirname(os.path.abspath(__file__))))
-
-    from models.info import *
+    from ingestion.models import *
 
 logger = logging.getLogger(__name__)
 
